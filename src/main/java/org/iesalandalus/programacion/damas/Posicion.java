e17d9fda2eec1a06b394bccf724abba685f5e481
jagmendez <joseagarciajgm@gmail.com>
2024-11-03 22:34:18 +0100

    Creado constructor para la clase "Posicion"

--- src/main/java/org/iesalandalus/programacion/damas/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/Posicion.java
@@ -5,6 +5,7 @@ public class Posicion {
     private char columna;
 
 
+    // Métodos getter y setter con validación y excepciones.
     public int getFila() { //Getter
         return fila;
     }
@@ -25,5 +26,18 @@ public class Posicion {
             throw new IllegalArgumentException("La columna debe estar entre 'a' y 'h'.")
         }
         this.columna = columna;
+    }
+
+
+    // Constructor
+    public Posicion(int fila, char columna) throws IllegalArgumentException{
+        if (fila < 1 || fila > 8) {
+            throw new IllegalArgumentException("La fila debe estar entre 1 y 8.");
+        }
+        if(columna < 'a' || columna > 'h') {
+            throw new IllegalArgumentException("La columna debe estar entre 'a' y 'h'.")
+        }
+        setFila(fila);
+        setColumna(columna);
     }
 }

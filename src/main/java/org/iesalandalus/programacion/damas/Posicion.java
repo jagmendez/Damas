36b025946264e50f6f4487f7d9cf5c831576e154
jagmendez <joseagarciajgm@gmail.com>
2024-11-03 13:35:36 +0100

    Añadidos métodos getter y setter para los atributos fila y columna, incluyendo validación y excepciones.

--- src/main/java/org/iesalandalus/programacion/damas/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/Posicion.java
@@ -3,4 +3,27 @@ package org.iesalandalus.programacion.damas;
 public class Posicion {
     private int fila;
     private char columna;
+
+
+    public int getFila() { //Getter
+        return fila;
+    }
+
+    public void setFila(int fila) { //Setter
+        if (fila < 1 || fila > 8) {
+            throw new IllegalArgumentException("La fila debe estar entre 1 y 8.");
+        }
+        this.fila = fila;
+    }
+
+    public char getColumna() { //Getter
+        return columna;
+    }
+
+    public void setColumna(char columna) { //Setter
+        if (columna < 'a' || columna > 'h') {
+            throw new IllegalArgumentException("La columna debe estar entre 'a' y 'h'.")
+        }
+        this.columna = columna;
+    }
 }

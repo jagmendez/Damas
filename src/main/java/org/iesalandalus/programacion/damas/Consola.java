29b91765317bf32d0ea976b5b6b8c263c4efdee8
jagmendez <joseagarciajgm@gmail.com>
2024-11-04 00:05:26 +0100

    Creado método mostrarMenu que muestra las opciones del menú en la clase "Consola".

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -6,4 +6,11 @@ public class Consola {
     private Consola() {
 
     }
+
+    public static void mostrarMenu() {
+        System.out.println("1. Crear dama por defecto");
+        System.out.println("1. Crear dama eligiendo su color");
+        System.out.println("3. Mover");
+        System.out.println("4. Salir");
+    }
 }

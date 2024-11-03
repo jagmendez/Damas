9703dd4ed2fedb9730fc52bae38b9b5bac32fd57
jagmendez <joseagarciajgm@gmail.com>
2024-11-04 00:14:06 +0100

    Creado método elegirOpcionMenu para solicitar una opción válida del menú en la clase "Consola".

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.damas;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
 
     // Constructor
@@ -7,10 +9,23 @@ public class Consola {
 
     }
 
+    //
     public static void mostrarMenu() {
         System.out.println("1. Crear dama por defecto");
         System.out.println("1. Crear dama eligiendo su color");
         System.out.println("3. Mover");
         System.out.println("4. Salir");
+    }
+
+    public static int elegirOpcionMenu() {
+        int opcionMenu;
+
+        // Comprobamos que la opción elegida es válida
+        do {
+            System.out.println("Elige una opción del menú (1-4): ");
+            opcionMenu= Entrada.entero();
+        }while(opcionMenu < 1 || opcionMenu > 4);
+
+        return opcionMenu;
     }
 }

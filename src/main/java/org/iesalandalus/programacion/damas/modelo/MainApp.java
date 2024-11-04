46eb8ed50cc4ea27349f5519cfa9f02d1cd3a241
jagmendez <joseagarciajgm@gmail.com>
2024-11-04 20:30:29 +0100

    //Creado el método "ejecutarOpcion" (MainApp)

--- src/main/java/org/iesalandalus/programacion/damas/modelo/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/MainApp.java
@@ -4,6 +4,28 @@ public class MainApp {
 
     //Creado atributo de clase Dama
     private static Dama dama;
+
+    //Creado método para ejecutar la opción pasada por parámetro según corresponda
+    public static void ejecutarOpcion(int opcion) {
+        switch (opcion) {
+            case 1:
+                crearDamaDefecto();
+                break;
+            case 2:
+                crearDamaColor();
+                break;
+            case 3:
+                mover();
+                break;
+            case 4:
+                mostrarDama();
+                break;
+            case 5:
+                System.out.println("Saliendo del juego... ¡Adios, hasta pronto!");
+            default:
+                System.out.println("Opción no válida.");
+        }
+    }
 	
 	
 }
